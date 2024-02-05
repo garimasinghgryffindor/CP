b567582cfb4dce11e1b320f3b9d8bb18e1a2e4da
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2024-02-05 15:54:22 +0530

    Update Two-Sets.cpp

--- CSES-problem-set/Two-Sets.cpp
+++ CSES-problem-set/Two-Sets.cpp
@@ -1,78 +1,85 @@
-#include <iostream>
-//#include <bits/stdc++.h>
-#include <vector>
-using namespace std;
+// FIRST APPROACH
+//*******************************
+// *************************
+// RTE
+//****************************
+// ***************************
 
-#define ll long long
-vector<vector<int> > mem;
+// #include <iostream>
+// //#include <bits/stdc++.h>
+// #include <vector>
+// using namespace std;
 
-bool recur(vector<int>&nums, ll oneSetSum, int idx, vector<bool>&vec) {
-    if(oneSetSum < 0) return false;
-    if(oneSetSum == 0) return true;
-    if(idx >= nums.size()) return false;
-    if(mem[idx][oneSetSum] == 2) return true;
-    if(mem[idx][oneSetSum] == 1) return false;
+// #define ll long long
+// vector<vector<int> > mem;
 
-    // not take it
-    vec[idx] = true;
-    bool ans = recur(nums, oneSetSum-nums[idx], idx+1, vec);
-    if(ans) { mem[idx][oneSetSum] = 2; return true; }
-    vec[idx] = false;
+// bool recur(vector<int>&nums, ll oneSetSum, int idx, vector<bool>&vec) {
+//     if(oneSetSum < 0) return false;
+//     if(oneSetSum == 0) return true;
+//     if(idx >= nums.size()) return false;
+//     if(mem[idx][oneSetSum] == 2) return true;
+//     if(mem[idx][oneSetSum] == 1) return false;
 
-    // take it
-    ans = recur(nums, oneSetSum, idx+1, vec);
-    if(ans) { mem[idx][oneSetSum] = 2; }
-    else { mem[idx][oneSetSum] = 1; }
-    return ans;
-}
+//     // not take it
+//     vec[idx] = true;
+//     bool ans = recur(nums, oneSetSum-nums[idx], idx+1, vec);
+//     if(ans) { mem[idx][oneSetSum] = 2; return true; }
+//     vec[idx] = false;
 
-int main()
-{
-    int n;
-    cin>>n;
+//     // take it
+//     ans = recur(nums, oneSetSum, idx+1, vec);
+//     if(ans) { mem[idx][oneSetSum] = 2; }
+//     else { mem[idx][oneSetSum] = 1; }
+//     return ans;
+// }
 
-    if(n == 1) {
-        cout<<"NO\n";
-    } else {
-        ll sum = (ll)n*(n+1)/2;
-        if(sum % 2 == 1) {
-            cout<<"NO\n";
-        } else {
-            ll oneSetSum = sum/2;
+// int main()
+// {
+//     int n;
+//     cin>>n;
 
-            vector<int>nums(n);
-            for(int i = 0; i < n; i++) {
-                nums[i] = i+1;
-            }
+//     if(n == 1) {
+//         cout<<"NO\n";
+//     } else {
+//         ll sum = (ll)n*(n+1)/2;
+//         if(sum % 2 == 1) {
+//             cout<<"NO\n";
+//         } else {
+//             ll oneSetSum = sum/2;
 
-            // 0/1 knapsack
-            vector<bool>vec(n, false);
-            mem.resize(n+1, vector<int>(oneSetSum+1, false));
-            bool ans = recur(nums, oneSetSum, 0, vec);
-            if(!ans) cout<<"NO\n";
-            else {
-                cout<<"YES\n";
-                int count = 0;
-                for(auto x: vec) {
-                    if(x) count++;
-                }
-                cout<<count<<endl;
-                for(int i = 0; i < n; i++) {
-                    if(vec[i]) {
-                        cout<<i+1<<" ";
-                    }
-                }
-                cout<<endl<<n-count<<endl;
-                for(int i = 0; i < n; i++) {
-                    if(!vec[i]) {
-                        cout<<i+1<<" ";
-                    }
-                }
-                cout<<endl;
-            }
-        }
+//             vector<int>nums(n);
+//             for(int i = 0; i < n; i++) {
+//                 nums[i] = i+1;
+//             }
+
+//             // 0/1 knapsack
+//             vector<bool>vec(n, false);
+//             mem.resize(n+1, vector<int>(oneSetSum+1, false));
+//             bool ans = recur(nums, oneSetSum, 0, vec);
+//             if(!ans) cout<<"NO\n";
+//             else {
+//                 cout<<"YES\n";
+//                 int count = 0;
+//                 for(auto x: vec) {
+//                     if(x) count++;
+//                 }
+//                 cout<<count<<endl;
+//                 for(int i = 0; i < n; i++) {
+//                     if(vec[i]) {
+//                         cout<<i+1<<" ";
+//                     }
+//                 }
+//                 cout<<endl<<n-count<<endl;
+//                 for(int i = 0; i < n; i++) {
+//                     if(!vec[i]) {
+//                         cout<<i+1<<" ";
+//                     }
+//                 }
+//                 cout<<endl;
+//             }
+//         }
         
-    }
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
